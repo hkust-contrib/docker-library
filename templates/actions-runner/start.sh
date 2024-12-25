e7438922b23894811bd31618cd85c393a7749f17
Kristopher <root@sandb0x.io>
2024-12-25 12:59:48 +0800

    added support for group runner registration

--- templates/actions-runner/start.sh
+++ templates/actions-runner/start.sh
@@ -2,15 +2,23 @@
 
 REPOSITORY=$REPO
 ACCESS_TOKEN=$TOKEN
+ORGANIZATION=$ORG
 
 echo "REPO ${REPOSITORY}"
 echo "ACCESS_TOKEN ${ACCESS_TOKEN}"
+echo "ORG ${ORGANIZATION}"
 
-REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+if [ -z "$ORGANIZATION" ]; then
+  REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/repos/${REPOSITORY}/actions/runners/registration-token | jq .token --raw-output)
+  URL="https://github.com/${REPOSITORY}"
+else
+  REG_TOKEN=$(curl -X POST -H "Authorization: token ${ACCESS_TOKEN}" -H "Accept: application/vnd.github+json" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
+  URL="https://github.com/${ORGANIZATION}"
+fi
 
 cd /home/runner/actions-runner
 
-./config.sh --url https://github.com/${REPOSITORY} --token ${REG_TOKEN}
+./config.sh --url ${URL} --token ${REG_TOKEN}
 
 cleanup() {
     echo "Removing runner..."
